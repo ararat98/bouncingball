0000380f1c9e148e9785d577d3b07804521be741
ararat98 <ararat.manukyan.98@gmail.com>
2023-12-17 16:13:20 +0400

    bouncing

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 var canvas, ctx, balls = [], lastTime = 0;
+var img = new Image();
+img.src = './ball.svg';
 var Ball = /** @class */ (function () {
     function Ball(x, y) {
         this.x = x;
         this.y = y;
-        this.radius = Math.floor((Math.random() + 0.5) * 26);
-        this.color = getRandomColor();
+        this.radius = (window.innerWidth / 100) * 2;
         this.velocityY = 0;
         this.damping = 0.9;
     }
     Ball.prototype.draw = function () {
-        if (ctx != undefined) {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
+        if (ctx !== undefined && ctx !== null) {
+            ctx.drawImage(img, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
         }
     };
     Ball.prototype.update = function (deltaTime) {
         this.velocityY += 9.8 * 0.01 * deltaTime;
         this.y += this.velocityY;
-        if (this.y + this.radius > canvas.height) {
-            this.y = canvas.height - this.radius;
+        if (this.y + this.radius > ((canvas === null || canvas === void 0 ? void 0 : canvas.height) || 0)) {
+            this.y = ((canvas === null || canvas === void 0 ? void 0 : canvas.height) || 0) - this.radius;
             this.velocityY *= -this.damping;
         }
         this.draw();
     };
     return Ball;
 }());
-function getRandomColor() {
-    return "#".concat(("0000" + ((Math.random() * 0x100) | 0).toString(16)).slice(-6));
-}
 function spawnBall(x, y) {
     balls.push(new Ball(x, y));
     if (balls.length > 10)
@@ -38,7 +32,7 @@ function spawnBall(x, y) {
 }
 function tick(currentTime) {
     var deltaTime = currentTime - lastTime;
-    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx === null || ctx === void 0 ? void 0 : ctx.clearRect(0, 0, (canvas === null || canvas === void 0 ? void 0 : canvas.width) || 0, (canvas === null || canvas === void 0 ? void 0 : canvas.height) || 0);
     balls.forEach(function (ball) { return ball.update(deltaTime); });
     lastTime = currentTime;
     requestAnimationFrame(tick);
@@ -46,12 +40,12 @@ function tick(currentTime) {
 function init() {
     canvas = document.getElementById("canvas");
     canvas === null || canvas === void 0 ? void 0 : canvas.addEventListener("click", function (event) {
-        var _a = canvas.getBoundingClientRect(), left = _a.left, top = _a.top;
+        var _a = canvas === null || canvas === void 0 ? void 0 : canvas.getBoundingClientRect(), left = _a.left, top = _a.top;
         spawnBall(event.clientX - left, event.clientY - top);
     });
     if (canvas) {
         canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight - 10;
+        canvas.height = window.innerHeight - 4;
         ctx = canvas.getContext("2d");
         requestAnimationFrame(tick);
     }
--- app.ts
+++ app.ts
@@ -1,77 +1,71 @@
-
 let canvas: HTMLCanvasElement | null,
-    ctx: CanvasRenderingContext2D | null,
-    balls: Ball[] = [],
-    lastTime = 0;
+  ctx: CanvasRenderingContext2D | null,
+  balls: Ball[] = [],
+  lastTime = 0;
+
+const img = new Image();
+img.src = './ball.svg';
 
 class Ball {
-    x: number;
-    y: number;
-    radius: number;
-    color: string;
-    velocityY: number;
-    damping: number;
+  x: number;
+  y: number;
+  radius: number;
+  velocityY: number;
+  damping: number;
 
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-        this.radius = Math.floor((Math.random() + 0.5) * 26);
-        this.color = getRandomColor();
-        this.velocityY = 0;
-        this.damping = 0.9;
-    }
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.radius = (window.innerWidth / 100) * 2;
+    this.velocityY = 0;
+    this.damping = 0.9;
+  }
 
-    draw(): void {
-        if (ctx != undefined) {
-            ctx.beginPath();
-            ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-            ctx.fillStyle = this.color;
-            ctx.fill();
-            ctx.closePath();
-        }
+  draw(): void {
+    if (ctx !== undefined && ctx !== null) {
+      ctx.drawImage(img, this.x - this.radius, this.y - this.radius, this.radius * 2, this.radius * 2);
     }
+  }
 
-    update(deltaTime: number): void {
-        this.velocityY += 9.8 * 0.01 * deltaTime;
-        this.y += this.velocityY;
-        if (this.y + this.radius > canvas!.height) {
-            this.y = canvas!.height - this.radius;
-            this.velocityY *= -this.damping;
-        }
-        this.draw();
+  update(deltaTime: number): void {
+    this.velocityY += 9.8 * 0.01 * deltaTime;
+    this.y += this.velocityY;
+    if (this.y + this.radius > (canvas?.height || 0)) {
+      this.y = (canvas?.height || 0) - this.radius;
+      this.velocityY *= -this.damping;
     }
+    this.draw();
+  }
 }
 
-function getRandomColor(): string {
-    return `#${("0000" + ((Math.random() * 0x100) | 0).toString(16)).slice(-6)}`;
-}
+
 
 function spawnBall(x: number, y: number): void {
-    balls.push(new Ball(x, y));
-    if (balls.length > 10) balls.shift();
+  balls.push(new Ball(x, y));
+  if (balls.length > 10) balls.shift();
 }
 
 function tick(currentTime: number): void {
-    let deltaTime = currentTime - lastTime;
-    ctx?.clearRect(0, 0, canvas!.width, canvas!.height);
-    balls.forEach((ball) => ball.update(deltaTime));
-    lastTime = currentTime;
-    requestAnimationFrame(tick);
+  const deltaTime = currentTime - lastTime;
+  ctx?.clearRect(0, 0, canvas?.width || 0, canvas?.height || 0);
+  balls.forEach((ball) => ball.update(deltaTime));
+  lastTime = currentTime;
+  requestAnimationFrame(tick);
 }
 
 function init(): void {
-    canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
-    canvas?.addEventListener("click", (event: MouseEvent) => {
-        let { left, top } = canvas!.getBoundingClientRect();
-        spawnBall(event.clientX - left, event.clientY - top);
-    });
+  canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  canvas?.addEventListener("click", (event) => {
+    const { left, top }:any = canvas?.getBoundingClientRect();
+    spawnBall(event.clientX - left, event.clientY - top);
+  });
 
-    if (canvas) {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight - 10;
-        ctx = canvas.getContext("2d") as CanvasRenderingContext2D | null;
-        requestAnimationFrame(tick);
-    }
+  if (canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight - 4;
+    ctx = canvas.getContext("2d");
+    requestAnimationFrame(tick);
+  }
 }
 
 init();
